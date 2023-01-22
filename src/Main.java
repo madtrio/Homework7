712e57dafc4e503b042585dca4c03d4d51452ce7
madtro <anna.kozitzyna@yandex.ru>
2023-01-22 17:49:56 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,26 @@ public class Main {
         task4();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Task 1");
-
+        int[] numb3;
+        numb3 = new int[]{1, 2, 3};
+        double[] numbFloat = {1.57, 7.654, 9.986};
+        int[] myNumb = new int[4];
+        myNumb = new int[]{3, 2, 1, 0};
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Task 2");
 
     }
 
-    public static void task3 () {
+    public static void task3() {
         System.out.println("Task 3");
 
     }
 
-    public static void task4 () {
+    public static void task4() {
         System.out.println("Task 4");
 
     }
